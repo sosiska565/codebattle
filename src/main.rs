e76d955583ac263ec88bdb3fa6524db506bde6f8
Egor <sosifka666@gmail.com>
2026-09-13 12:40:36 +0300

    add dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use axum::{Router, response::Html, routing::get};
+mod repository;
+
+use axum::{
+    Router,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(handler));
+
+    let app = app.fallback(handler_404);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -14,4 +23,13 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>hello world</h1>")
+}
+
+async fn handler_404() -> impl IntoResponse {
+    (
+        StatusCode::NOT_FOUND,
+        Html(
+            "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ36o0llmjMX_fETdo_9z27x20N85tVCFnd18Y3jl32iQ&s\">",
+        ),
+    )
 }
---
+++ src/repository/user.rs
@@ -0,0 +1,8 @@
+#[derive(Default, Debug)]
+pub struct User {
+    pub id: i64,
+    pub name: String,
+    pub elo: i32,
+}
+
+impl User {}
